e51f51cced0399b69e15699197e484d26d809edd
Uherkovich Péter <uherkovich.peter@pte.hu>
2017-05-03 15:22:13 +0200

    assets - less

---
+++ src/UMenuAsset.php
@@ -0,0 +1,12 @@
+<?php
+namespace uhi67\umenu;
+
+use yii\web\AssetBundle;
+
+class UMenuAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/uhi67/umenu/src/assets';
+    public $css = [
+        'umenu.css',
+    ];
+}
